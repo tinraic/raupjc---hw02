35bbf426b8ba64e6232836414cf02fe91c911b82
Tin Raic <tin.raic@fer.hr>
2017-11-06 19:14:14 +0100

    Updated

--- Task_4/HomeworkLinqQueries.cs
+++ Task_4/HomeworkLinqQueries.cs
@@ -10,8 +10,8 @@ namespace Task_4
     {
         public static string[] Linq1(int[] intArray)
         {
-            var result = intArray.GroupBy(i => i).Select(t => new string[2] {t.Key.ToString(), t.Count().ToString()})
-                .OrderBy(num => num[0]).Select(i => "Broj" + i[0] + " ponavlja se " + i[1] + " puta");
+            var result = intArray.GroupBy(i => i).Select(t => new string[2] { t.Key.ToString(), t.Count().ToString() })
+                .OrderBy(num => num[0]).Select(i => "broj " + i[0] + " ponavlja se " + i[1] + " puta");
 
             return result.ToArray();
         }
@@ -20,15 +20,15 @@ namespace Task_4
 
         public static University[] Linq2_2(University[] universityArray)
         {
-            float average = universityArray.Select(s => new object[2] {s.Name, s.Students.Length})
-                .Average(uni => (float) uni[1]);
+            float average = universityArray.Select(s => new object[2] { s.Name, s.Students.Length })
+                .Average(uni => float.Parse(uni[1].ToString()));
 
             return universityArray.Where(uni => uni.Students.Length < average).ToArray();
         }
 
         public static Student[] Linq2_3(University[] universityArray) =>
             universityArray.SelectMany(t => t.Students).Distinct().ToArray();
-    
+
         public static Student[] Linq2_4(University[] universityArray)
         {
             Student[] maleUniversitiesStudents = universityArray
@@ -44,9 +44,15 @@ namespace Task_4
 
         }
 
-        public static Student[] Linq2_5(University[] universityArray) => universityArray.SelectMany(uni => uni.Students)
-            .GroupBy(s => s.Jmbag).Select(s => new object[2] {s, s.Count()}).Where(s => (int) s[1] > 1)
-            .Select(s => (Student) s[0]).Distinct().ToArray();
+        public static Student[] Linq2_5(University[] universityArray)
+        {
+            List<string> moreThanOneCollegeJmbagsList = universityArray.SelectMany(t => t.Students).GroupBy(x => x.Jmbag).Select(x => new
+            {
+                x.Key,
+                Count = x.Count()
+            }).Where(t => (int)t.Count > 1).Select(i => i.Key).ToList();
 
+            return universityArray.SelectMany(uni => uni.Students).Distinct().Where(s => moreThanOneCollegeJmbagsList.Contains(s.Jmbag)).ToArray();
+        }
     }
 }
---
+++ Task_5/readme.md
@@ -0,0 +1,17 @@
+﻿#Pitanje 1
+Izvođenje programa trajalo je 5,0058782 sekundi.
+
+#Pitanje 2
+Operacije A, B, C, D i E izvodile su se na jednoj dretvi.
+
+#Pitanje 3
+Izvođenje programa trajalo je 1,0689109 sekundi.
+
+#Pitanje 4
+Operacije A, B, C, D i E izvodile su se na 5 dretvi.
+
+#Pitanje 5
+Do neželjenog ponašanja može doći primjerice kada jedna dretva dohvati varijablu i uveća 
+njenu vrijednost u istom trenutku kad i neka druga dretva. To će rezultirati time da je ta
+varijabla uvećana samo za jedan, a ne za dva kako bi u ovo slučaju trebalo biti (primjerice
+da se operacija izvodi slijedno ili da su dretve dohvatile varijablu u različitim trenutcima).
